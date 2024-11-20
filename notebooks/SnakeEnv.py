06f5a061815e9106dfc2a2b71b38865dfa22b191
RomaanMkv <romanmakeev519@gmail.com>
2024-11-20 13:19:50 +0300

    distance reward has been added

--- notebooks/SnakeEnv.py
+++ notebooks/SnakeEnv.py
@@ -4,13 +4,15 @@ from gymnasium import spaces
 import random
 
 class SnakeEnv(gym.Env):
-    def __init__(self, grid_size=10):
+    def __init__(self, grid_size=10, food_reward=1, collision_reward=-1):
         super(SnakeEnv, self).__init__()
         self.grid_size = grid_size
         self.action_space = spaces.Discrete(4)  # 0: Up, 1: Right, 2: Down, 3: Left
         self.observation_space = spaces.Box(0, 1, (grid_size, grid_size, 3), dtype=np.float32)
         self.reset()
         self.seed()
+        self.food_reward = food_reward
+        self.collision_reward = collision_reward
     
     def seed(self, seed=None):
         random.seed(seed)
@@ -23,6 +25,7 @@ class SnakeEnv(gym.Env):
         self.food = self._place_food()
         self.direction = 1  # Initial direction: Right
         self.done = False
+        self.previous_distance = self._calculate_distance(self.snake[0], self.food)
         return self._get_observation()
 
     def _place_food(self):
@@ -35,6 +38,10 @@ class SnakeEnv(gym.Env):
             obs[x, y] = [0, 1, 0]  # Snake body in green
         obs[self.food[0], self.food[1]] = [1, 0, 0]  # Food in red
         return obs
+
+    def _calculate_distance(self, position, food_position):
+        """Calculate Manhattan distance between the snake's head and the food."""
+        return abs(position[0] - food_position[0]) + abs(position[1] - food_position[1])
 
     def step(self, action):
         if self.done:
@@ -70,7 +77,7 @@ class SnakeEnv(gym.Env):
             new_head in self.snake
         ):
             self.done = True
-            return self._get_observation(), -1, self.done, {}
+            return self._get_observation(), self.collision_reward, self.done, {}
 
         # Update snake position
         self.snake.insert(0, new_head)
@@ -81,13 +88,21 @@ class SnakeEnv(gym.Env):
             reward = 100
             return self._get_observation(), reward, self.done, {}
 
+        # Calculate distance-based reward
+        current_distance = self._calculate_distance(new_head, self.food)
+        distance_reward = self.previous_distance - current_distance
+
         if new_head == self.food:
-            reward = 1
+            reward = self.food_reward + distance_reward  # Extra reward for eating food
             self.food = self._place_food()  # Place new food
+            self.previous_distance = self._calculate_distance(new_head, self.food)
         else:
             tail = self.snake.pop()
             self.free_cells.add(tail)
-            reward = 0
+            reward = distance_reward
+
+        # Update the previous distance
+        self.previous_distance = current_distance
 
         return self._get_observation(), reward, self.done, {}
 
