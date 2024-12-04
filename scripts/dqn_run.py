842d25fda448324b26fde7d2cafb05935b372ad3
Ivan Golov <ivangolov@icloud.com>
2024-12-04 12:39:56 +0300

    update DQN model with stats

--- scripts/dqn_run.py
+++ scripts/dqn_run.py
@@ -9,6 +9,5 @@ if __name__ == '__main__':
     env = ParObsSnakeEnv(grid_size=20, interact=True)
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.n
-    agent = DQNAgent(state_dim, action_dim)
-    agent.load('models/dqn/dqn_agent_par_300_10.pkl')
+    agent = DQNAgent.load('models/dqn/dqn_agent_par_300_10.pkl')
     run_simulation(agent, env, num_simulations=1)
